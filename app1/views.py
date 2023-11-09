539dcf099f2d4a0aa5f77c29a42368775e36fc17
AnuBTS123 <anuhyareddivari@gmail.com>
2023-11-09 18:56:25 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Abi(request):
+    return render(request,'Abi.html')
+
+def Anu(request):
+    return render(request,'Anu.html')
+
+def Pandu(request):
+    return render(request,'Pandu.html')
+
+def Shivani(request):
+    return render(request,'Shivani.html')
